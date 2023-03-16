c89af9d635e7e10103fac53bec6654e3ceddf0f8
Jaehyun Kim <rlapo@naver.com>
2023-03-16 17:22:57 +0900

    .

---
+++ 1/2.c
@@ -0,0 +1,30 @@
+﻿#include<stdio.h>
+void ABC(int a[], int k) {
+	int mx = a[9];
+	int mxidx = 9;
+	for (int i = k; i < 10; i++) {
+		if (a[i] > mx) {
+			mx = a[i];
+			mxidx = i;
+		}
+	}
+
+	int t = a[k];
+	a[k] = a[mxidx];
+	a[mxidx] = t;
+
+}
+int main() {
+	int a[10];
+	for (int i = 0; i < 10; i++) {
+		scanf("%d", &a[i]);
+	}
+
+	for (int i = 0; i < 10; i++) {
+		ABC(a, i);
+	}
+	for (int i = 0; i < 10; i++) {
+		printf("%d ", a[i]);
+	}
+	return 0;
+}
---
+++ 2/2.c
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+int mostOnes(int a[101][101], int n) {
+	int ans = 0;
+	int c = 0;
+	for (int i = 0; i < n; i++)
+		for (; c < n && a[i][c] == 1; c++)
+			ans = i;
+	return ans;
+}
+int main() {
+	int n;
+	int a[101][101];
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			scanf("%d", &a[i][j]);
+	printf("%d", mostOnes(a, n));
+}
