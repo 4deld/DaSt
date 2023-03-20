f63eb0513d7e94ce118b7149af3b81949e2be4ff
Jaehyun Kim <rlapo@naver.com>
2023-03-20 18:58:33 +0900

    Create 2.c

---
+++ 3/2.c
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+void ftn(int a) {
+	if (a == 0) {
+		return;
+	}
+	ftn(a / 10);
+	printf("%d\n", a % 10);
+}
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	ftn(n);
+	return 0;
+}
